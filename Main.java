08eeb67eb07ca21d17f2f243dd8763edcc3e2c85
tehoblivious <nhall14@students.chattahoocheetech.edu>
2020-09-03 11:40:14 -0400

    Added a java file and updated index

---
+++ Main.java
@@ -0,0 +1,14 @@
+//package io.github.jiangdequan; //automatically created, hmmph. 
+
+public class Main {
+    public static void main(String[] args) {
+         //so we just did an f1 command palette in vs code 
+         //and it made a new java class file, NICE 
+         //then typed in main or whatever and got a free main method to use 
+
+         System.out.println("//then we type sysout and get this easily");
+    }
+}
+
+
+//then CTRL-F11 in vs code will show java output really nicely ("run code", that extension I believe)
